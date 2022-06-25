89247650836062655b76e07b4623e19fdce0dd51
Enming Hu <enming.hu@china.talkdesk.com>
2022-06-25 13:56:55 +0800

    fix: add status to control bar

--- src/components/ControlBar/index.js
+++ src/components/ControlBar/index.js
@@ -1,4 +1,6 @@
 import { Space, Typography } from "antd";
+import { Row } from "antd";
+import { Badge } from "antd";
 import {
   SyncOutlined,
   StepBackwardOutlined,
@@ -6,16 +8,53 @@ import {
   StepForwardOutlined,
 } from "@ant-design/icons";
 
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
-export default function ControlBar({ time = "03:23:08 PM" }) {
+const statusColorMap = {
+  waiting: {
+    text: "Wait to start",
+    statusColor: "#8c8c8c",
+    textColor: "#d9d9d9",
+  },
+  running: {
+    text: "Simulating",
+    statusColor: "red",
+    textColor: "black",
+  },
+};
+
+export default function ControlBar({
+  time = "03:23:08 PM",
+  status = "waiting",
+}) {
   return (
-    <Space size="large" style={{ fontSize: 18 }}>
-      <Text strong>{time}</Text>
-      <StepBackwardOutlined onClick={() => {}} />
-      <PauseOutlined onClick={() => {}} />
-      <StepForwardOutlined onClick={() => {}} />
-      <SyncOutlined onClick={() => {}} />
-    </Space>
+    <>
+      <div style={{ color: statusColorMap[status].textColor }}>
+        <Row justify="space-between" style={{ padding: "0 8px" }}>
+          <Title level={4}>Simulation Result</Title>
+          <Badge
+            color={statusColorMap[status].statusColor}
+            text={statusColorMap[status].text}
+            style={{
+              color:
+                status === "running"
+                  ? statusColorMap[status].statusColor
+                  : statusColorMap[status].textColor,
+            }}
+          />
+        </Row>
+        <Row justify="end" style={{ padding: "0 8px" }}>
+          <Space size="large" style={{ fontSize: 18 }}>
+            <Text strong style={{ color: statusColorMap[status].textColor }}>
+              {time}
+            </Text>
+            <StepBackwardOutlined onClick={() => {}} />
+            <PauseOutlined onClick={() => {}} />
+            <StepForwardOutlined onClick={() => {}} />
+            <SyncOutlined onClick={() => {}} />
+          </Space>
+        </Row>
+      </div>
+    </>
   );
 }
