0c5dc3509d8522f8b43208c839f30fd6a0abe6e7
TD-RobinRen <bin.ren@china.talkdesk.com>
2022-06-26 19:08:50 +0800

    feat: Add reset

--- src/hooks/use-store.js
+++ src/hooks/use-store.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { createContainer } from "unstated-next";
 
-const defaultBaseData = {
+export const defaultBaseData = {
   current_time: Date.now(),
   start_date: Date.now(),
   service_level: 10,
@@ -133,7 +133,7 @@ function useGlobalStore(
     baseData,
     setBaseData,
     offset,
-    setOffset
+    setOffset,
   };
 }
 
